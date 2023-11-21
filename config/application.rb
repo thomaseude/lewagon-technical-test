bd8f683c5193f9327a928d341c0106eb58ed56aa
Dimitri Bosch <dimitribosch@hotmail.com>
2023-11-21 15:54:16 +0100

    Setup generators settings

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module HiringPairprogrammingTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
   end
 end
