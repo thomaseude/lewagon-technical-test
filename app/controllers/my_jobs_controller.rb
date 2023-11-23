58c21072be64360bdaef6cd79b7b2b8cba51efd2
Dimitri Bosch <dimitribosch@hotmail.com>
2023-11-23 15:15:27 +0100

    Add job edition

--- app/controllers/my_jobs_controller.rb
+++ app/controllers/my_jobs_controller.rb
@@ -15,4 +15,23 @@ class MyJobsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @job = current_user.jobs.find(params[:id])
+  end
+
+  def update
+    @job = current_user.jobs.find(params[:id])
+    if @job.update(job_params)
+      redirect_to my_jobs_path, notice: "Job updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :status)
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,15 @@ class Job < ApplicationRecord
   belongs_to :user
 
   enum status: [:draft, :open, :closed]
+
+  before_save :set_opened_at, if: :open?
+  before_save :set_closed_at, if: :closed?
+
+  def set_opened_at
+    self.opened_at = Time.zone.now
+  end
+
+  def set_closed_at
+    self.closed_at = Time.zone.now
+  end
 end
