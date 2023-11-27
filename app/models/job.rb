23655126ac4c57aeab3940cee10fb761cb8c3fa6
Dimitri Bosch <dimitribosch@hotmail.com>
2023-11-27 16:17:59 +0100

    Fix callbacks

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,10 +7,10 @@ class Job < ApplicationRecord
   before_save :set_closed_at, if: :closed?
 
   def set_opened_at
-    self.opened_at = Time.zone.now
+    self.opened_at ||= Time.zone.now
   end
 
   def set_closed_at
-    self.closed_at = Time.zone.now
+    self.closed_at ||= Time.zone.now
   end
 end
